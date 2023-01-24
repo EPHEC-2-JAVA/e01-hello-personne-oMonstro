349fed252faf180de3c2311dc96b7f42174f891c
Babadi Mukanya <psr08035@students.ephec.be>
2023-01-24 23:40:31 +0100

    Exercice 2 fait dans le main

--- src/main/java/be/ephec/java/exercice/Main.java
+++ src/main/java/be/ephec/java/exercice/Main.java
@@ -10,23 +10,40 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        // Exercice 1
         Person person1 = new Person();
-
         String ageAsString;
-
         System.out.println("Quel est votre prenom?");
         person1.firstName = scan.next();
-
         System.out.println("Quel est votre nom?");
         person1.lastName = scan.next();
-
         System.out.println("Quel est votre age?");
         ageAsString = scan.next();
         person1.age = Integer.parseInt(ageAsString);
-
         person1.display();
 
-        //System.out.println("Nom = " + person1.lastName + " prenom = " + person1.firstName + " age = " + person1.age);
+        //Exercice 2
+        int inputAsInt1, radixAsInt;
+        String inputAsString1, radixAsString;
+
+        try{
+            do {
+                System.out.println("Donnez un nombre et puis sa base :");
+                inputAsString1 = scan.next();
+                radixAsString = scan.next();
+                radixAsInt = Integer.parseInt(radixAsString);
+                inputAsInt1 = Integer.parseInt(inputAsString1);
+                System.out.println("Executer Base avec " + inputAsInt1 + " et " + radixAsInt + " donnera " + Integer.parseInt(inputAsString1, radixAsInt));
+
+            }while((inputAsInt1 != 0 || inputAsInt1 != Character.MIN_RADIX || inputAsInt1 != Character.MAX_RADIX) && radixAsInt != 0);
+
+        } catch (NumberFormatException e){
+            System.out.println("O inputed, end of program");
+        }
+
+
+
+
 
 
 
