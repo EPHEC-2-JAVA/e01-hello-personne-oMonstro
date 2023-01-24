d4e47abb2770c90c89171e1d266f1e7b800dda5f
Babadi Mukanya <psr08035@students.ephec.be>
2023-01-24 20:55:02 +0100

    Exercice fait

--- src/main/java/be/ephec/java/exercice/Main.java
+++ src/main/java/be/ephec/java/exercice/Main.java
@@ -1,7 +1,34 @@
 package be.ephec.java.exercice;
 
+
+import be.ephec.java.exercice.e01.Person;
+
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        Person person1 = new Person();
+
+        String ageAsString;
+
+        System.out.println("Quel est votre prenom?");
+        person1.firstName = scan.next();
+
+        System.out.println("Quel est votre nom?");
+        person1.lastName = scan.next();
+
+        System.out.println("Quel est votre age?");
+        ageAsString = scan.next();
+        person1.age = Integer.parseInt(ageAsString);
+
+        person1.display();
+
+        //System.out.println("Nom = " + person1.lastName + " prenom = " + person1.firstName + " age = " + person1.age);
+
+
+
     }
 }
