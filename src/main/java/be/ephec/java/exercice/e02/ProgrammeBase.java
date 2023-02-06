e766aeec7614720a4f3438bf57a81ac39da6f83a
Babadi Mukanya <psr08035@students.ephec.be>
2023-02-06 22:02:53 +0100

    Correction exercice 2 suite aux feedbacks

---
+++ src/main/java/be/ephec/java/exercice/e02/ProgrammeBase.java
@@ -0,0 +1,10 @@
+package be.ephec.java.exercice.e02;
+
+public class ProgrammeBase {
+
+    public static void main(String[] args) {
+
+        Base test1 = new Base();
+        test1.ConversionDeBase();
+    }
+}
